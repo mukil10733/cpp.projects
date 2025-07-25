d2761defde4ced6acb44c1564ee513a78df440e5
mukil10733 <kambojmukil341@gmail.com>
2025-07-25 16:14:06 +0530

    added 3 files

---
+++ charDataType.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cin>>ch;
+    int x = (int)ch;
+    cout<<x;
+    return 0;
+}
---
+++ modulus.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"x : ";
+    cin>>x;
+    cout<<"y : ";
+    cin>>y;
+    cout<<"modulus : "<<x%y;
+    return 0;
+}
